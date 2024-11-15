1ca6462e782435234a42b8726e17e0269b651024
Cam <cammclain@protonmail.com>
2024-11-16 10:31:13 +1300

    add a bunch of components

---
+++ src/client/desktop/frontend/src/lib/auth.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export function useAuth() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    return { isAuthenticated, setIsAuthenticated };
+}
+
+
+// TODO: Add authentication with the team server 
+// / This auth should be similar to Starkiller's auth with the Empire server.
+// / - User, Password, URL of team server
+// / - Save the user's JWT in LocalStorage?
+
+
+
+function clientLogin() {
+    // TODO: Implement login
+    // / - should provide the user with a login prompt
+    // / - this form should send an HTTP POST request to the team server's `/login` endpoint
+    // / - if the login is successful, the user's JWT should be saved in LocalStorage
+    // / - if the login is successful, the user should be redirected to the dashboard
+    // / - if the login fails, the user should be notified with an error message
+}
+
+function clientLogout() {
+    // TODO: Implement logout
+    // / - should provide the user with a logout prompt
+    // / - this form should send an HTTP POST request to the team server's `/logout` endpoint
+    // / - if the logout is successful, the user's JWT should be removed from LocalStorage
+    // / - if the logout fails, the user should be notified with an error message
+}
+
